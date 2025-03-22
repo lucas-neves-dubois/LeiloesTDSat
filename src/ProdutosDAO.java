d63113f957a61aa9f569027e61417e3f92a8df25
Lucas Neves <lucas.n.dubois@gmail.com>
2025-03-22 16:58:31 -0300

    Botão de salvar cadastrando produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -8,33 +8,59 @@
  * @author Adm
  */
 
+
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+
+        conn = new conectaDAO().connectDB();
+
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+        try {
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+
+            prep.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Produto Cadastrado com sucesso!"); // Feedback
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + ex.getMessage());
+        } finally {
+            try {
+                if (prep != null) {
+                    prep.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
+
     
     
         
